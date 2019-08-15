0c0d3f870637e852aabfa97381664532d79a0fdd
tangkuo <616507752@qq.com>
2019-08-15 23:04:58 +0800

    consumer-user新增修改

---
+++ src/main/java/com/tk/ticket/service/TicketService.java
@@ -0,0 +1,6 @@
+package com.tk.ticket.service;
+
+public interface TicketService {
+
+    public String getTicket();
+}
---
+++ src/main/java/com/tk/user/service/UserService.java
@@ -0,0 +1,21 @@
+package com.tk.user.service;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.tk.ticket.service.TicketService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService{
+
+    @Reference
+    TicketService ticketService;
+
+    public void hello(){
+        String ticket = ticketService.getTicket();
+        System.out.println("买到票了："+ticket);
+    }
+
+
+}
